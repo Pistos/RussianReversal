b3a93f759f1fcc1b213abecd0cde50db0075b9ad
Pistos <gitsomegrace.5.pistos@geoshell.com>
2010-11-17 14:41:44 -0500

    Added some more test cases.

--- spec/reverse.rb
+++ spec/reverse.rb
@@ -8,5 +8,7 @@ describe 'RussianReversal' do
     RussianReversal.reverse( 'He fired the rocket' ).should.equal 'rockets fire'
     RussianReversal.reverse( 'The cats chased the mice' ).should.equal 'mice chase'
     RussianReversal.reverse( 'The cat chases the mouse' ).should.equal 'mice chase'
+    RussianReversal.reverse( 'The tanks decimated the platoon.' ).should.equal 'platoons decimate'
+    RussianReversal.reverse( 'The chef prepared a delicious meal.' ).should.equal 'meals prepare'
   end
 end
