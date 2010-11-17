ad879b86336941be03a55cdbcebb7e8eac0841bf
Pistos <gitsomegrace.5.pistos@geoshell.com>
2010-11-17 14:30:41 -0500

    Change some double quotes to single quotes.

--- spec/reverse.rb
+++ spec/reverse.rb
@@ -2,9 +2,9 @@ require 'spec/preparation'
 
 describe 'RussianReversal' do
   it 'can make the direct object the subject' do
-    RussianReversal.reverse( "They help Americans" ).should.equal 'Americans help'
-    RussianReversal.reverse( "He eliminated enemies" ).should.equal 'enemies eliminate'
-    RussianReversal.reverse( "He shot an apple" ).should.equal 'apples shoot'
-    RussianReversal.reverse( "He fired the rocket" ).should.equal 'rockets fire'
+    RussianReversal.reverse( 'They help Americans' ).should.equal 'Americans help'
+    RussianReversal.reverse( 'He eliminated enemies' ).should.equal 'enemies eliminate'
+    RussianReversal.reverse( 'He shot an apple' ).should.equal 'apples shoot'
+    RussianReversal.reverse( 'He fired the rocket' ).should.equal 'rockets fire'
   end
 end
