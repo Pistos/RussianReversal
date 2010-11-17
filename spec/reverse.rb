6099357aaabcbfa1d1aefcfaa91f98eda99aa7d3
Pistos <gitsomegrace.5.pistos@geoshell.com>
2010-11-17 14:30:23 -0500

    Added more test cases.

--- spec/reverse.rb
+++ spec/reverse.rb
@@ -1,8 +1,10 @@
 require 'spec/preparation'
 
 describe 'RussianReversal' do
-  it 'can identify the direct object' do
+  it 'can make the direct object the subject' do
     RussianReversal.reverse( "They help Americans" ).should.equal 'Americans help'
     RussianReversal.reverse( "He eliminated enemies" ).should.equal 'enemies eliminate'
+    RussianReversal.reverse( "He shot an apple" ).should.equal 'apples shoot'
+    RussianReversal.reverse( "He fired the rocket" ).should.equal 'rockets fire'
   end
 end
